9ce0a5d7a05f9a33238da0a970112d34874b4425
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2023-09-16 14:18:18 -0700

    l

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -1,2 +1,6 @@
 class Cache < ApplicationRecord
+  enum data_type: {
+    locations: 0,
+    availabilities: 1,
+  }
 end
---
+++ lib/tasks/estimate.rake
@@ -0,0 +1,7 @@
+require 'csv'
+
+namespace :estimate do
+  task vaccines: :environment do
+    puts "Hello"
+  end
+end
